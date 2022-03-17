56f98227d99562638652b72098fa40a555e15898
Freja <freja.hyldgaard@gmail.com>
2022-03-17 21:25:49 +0100

    mongoDB filter

--- quiz2/routes/index.js
+++ quiz2/routes/index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const { regexpToText } = require("nodemon/lib/utils");
 var router = express.Router();
 const controller = require("../controllers/controller");
 
@@ -12,14 +13,26 @@ router.get("/create", function (req, res, next) {
   res.render("create", { title: "Create Quiz" });
 });
 
-/* GET show quiz page */
-// router.get("/show", function (req, res, next) {
-//   res.render("show", { title: "Show Quiz" });
-// });
+// Quiz1 = _id: "623355f2c0642195b43f0d1d"
+// Quiz2 = _id: "6233564bc0642195b43f0d26"
 
 /* GET try quiz page */
-router.get("/try", function (req, res, next) {
-  res.render("try", { title: "Try Quiz" });
+router.get("/try", async function (req, res, next) {
+  let quizzes = await controller.getQuiz({}, { sort: { title: 1 } });
+  // console.log(quizzes);
+  // console.log(quizzes[0]._id);
+  let questions = await controller.getQuestion({quiz: quizzes[0]._id}, { sort: { title: 1 } });
+  // console.log(questions);
+  let answers = await controller.getAnswer({question: questions[0]._id}, { sort: { answers: 1 } });
+  // console.log(answers);
+
+  res.render("try", {
+    title: "Try Quiz",
+    quizzes,
+    questions,
+    answers,
+  });
+
 });
 
 /* POST Quiz */
@@ -33,7 +46,7 @@ router.post("/create", async function (req, res, next) {
 /* GET Show quiz pag */
 router.get("/show", async function (req, res, next) {
   let quizzes = await controller.getQuiz({}, { sort: { title: 1 } });
-  let questions = await controller.getQuestion({_id:quizzes._id}, { sort: { title: 1 } });
+  let questions = await controller.getQuestion({}, { sort: { title: 1 } });
 
   let answers = await controller.getAnswer({}, { sort: { answers: 1 } });
 
